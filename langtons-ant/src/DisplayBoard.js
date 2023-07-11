e77aaf708721e75f867d04a9957561d46cc6c880
William J Squires <william.squiresii@gmail.com>
2023-07-11 14:22:20 -0500

    Remove ants prop. They are now included inside Board.

--- langtons-ant/src/DisplayBoard.js
+++ langtons-ant/src/DisplayBoard.js
@@ -14,11 +14,10 @@ const initialBoardDims = [-10,10,-10,10]
  *  boardDims: Array [xMin,xMax,yMin,yMax] of board dimenstions
  *  canvas: reference to the canvas that is to be updated.
  *
- * @param {Array} ants
  * @param {Board} boards
  */
 
-function DisplayBoard(ants, board) {
+function DisplayBoard(board) {
   const [boardDims, setBoardDims] = useState(initialBoardDims);
   const [canvas, setCanvas] = useState(null);
 
@@ -28,23 +27,28 @@ function DisplayBoard(ants, board) {
 
   if (canvas) {
     const ctx = canvas.getContext("2d");
-    ctx.lineWidth= 2
-    let numVerticalLines = Math.abs(boardDims[0]) + Math.abs(boardDims[1]) + 1
-    let numHorizontalLines = Math.abs(boardDims[2]) + Math.abs(boardDims[3]) + 1
-    for (let i = 0; i <= numHorizontalLines; i++) {
-      ctx.beginPath()
-      ctx.moveTo(0, 1000/numHorizontalLines*i)
-      ctx.lineTo(1000, 1000/numHorizontalLines*i)
-      ctx.stroke()
-    }
-    for (let i = 0; i <= numVerticalLines; i++) {
-      ctx.beginPath()
-      ctx.moveTo(1000/numVerticalLines*i, 0)
-      ctx.lineTo(1000/numVerticalLines*i, 1000)
-      ctx.stroke()
-    }
-    ctx.rect(1000/numVerticalLines*5,1000/numHorizontalLines*5,1000/numHorizontalLines,1000/numVerticalLines)
-    ctx.fill()
+
+
+
+
+
+    // ctx.lineWidth= 2
+    // let numVerticalLines = Math.abs(boardDims[0]) + Math.abs(boardDims[1]) + 1
+    // let numHorizontalLines = Math.abs(boardDims[2]) + Math.abs(boardDims[3]) + 1
+    // for (let i = 0; i <= numHorizontalLines; i++) {
+    //   ctx.beginPath()
+    //   ctx.moveTo(0, 1000/numHorizontalLines*i)
+    //   ctx.lineTo(1000, 1000/numHorizontalLines*i)
+    //   ctx.stroke()
+    // }
+    // for (let i = 0; i <= numVerticalLines; i++) {
+    //   ctx.beginPath()
+    //   ctx.moveTo(1000/numVerticalLines*i, 0)
+    //   ctx.lineTo(1000/numVerticalLines*i, 1000)
+    //   ctx.stroke()
+    // }
+    // ctx.rect(1000/numVerticalLines*5,1000/numHorizontalLines*5,1000/numHorizontalLines,1000/numVerticalLines)
+    // ctx.fill()
   }
 
 
