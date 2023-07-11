948f84900530cec6f00b03290aa979105bd14d4d
William J Squires <william.squiresii@gmail.com>
2023-07-11 14:21:26 -0500

    Add board state to app

--- langtons-ant/src/App.js
+++ langtons-ant/src/App.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import { Board } from "./models";
 import DisplayBoard from "./DisplayBoard";
-
+/** Top level app component.\
+ *
+ * Props: None
+ *
+ * State:
+ *  board: Board class
+ */
 function App() {
+  const [board, setBoard] = useState(new Board(1000,1000,-10,10,10,-10))
   return (
     <div className="App">
-      <DisplayBoard></DisplayBoard>
+      <DisplayBoard board={board}></DisplayBoard>
     </div>
   );
 }
